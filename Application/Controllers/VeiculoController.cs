8379f43f6730a07de7c7096902690dfda09f6a60
ViegasLucas <matheusviegaslucas@gmail.com>
2024-01-03 22:16:38 -0300

    Criando controller, enums e entidade

---
+++ Application/Controllers/VeiculoController.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Commands;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Application.Controllers
+{
+    [Route("api/[controller]")]
+    public class VeiculoController : ControllerBase
+    {
+        [HttpPost("CadastrarVeiculo")]
+        public IActionResult PostAsync([FromBody] VeiculoCommand command)
+        {
+            return Ok();
+        }
+
+        public IActionResult Simularaluguel()
+        {
+            return Ok();
+        }
+        public IActionResult alugarVeiculo()
+        {
+            return Ok();
+        }
+    }
+}
--- Service/obj/Debug/net8.0/Service.AssemblyInfo.cs
+++ Service/obj/Debug/net8.0/Service.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d05150698f3bfa2240a2d178c16cd1add3298a44")]
 [assembly: System.Reflection.AssemblyProductAttribute("Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
